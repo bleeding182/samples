e5deec7cdd1a35bebd30ecb060a15f2c784b0a62
David Medenjak <davidmedenjak@gmail.com>
2018-05-10 21:42:51 +0200

    add more options

--- RedditSample/auth/src/main/java/com/davidmedenjak/auth/OAuthAccountManager.java
+++ RedditSample/auth/src/main/java/com/davidmedenjak/auth/OAuthAccountManager.java
@@ -4,11 +4,15 @@ import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.accounts.AuthenticatorException;
 import android.accounts.OperationCanceledException;
+import android.os.Build;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import com.davidmedenjak.auth.api.model.TokenPair;
 
 import java.io.IOException;
+import java.util.Set;
 
 import javax.inject.Inject;
 
@@ -37,13 +41,58 @@ public class OAuthAccountManager implements AccountAuthenticator {
         }
 
         final String accessToken = token.accessToken;
-        accountManager.setAuthToken(account, "bearer", accessToken);
+        accountManager.setAuthToken(account, Token.BEARER, accessToken);
+    }
+
+    public void logout() {
+        if (account == null) return;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+            accountManager.removeAccount(account, null, null, null);
+        } else {
+            accountManager.removeAccount(account, null, null);
+        }
+    }
+
+    public boolean isLoggedIn() {
+        return account != null;
+    }
+
+    @Nullable
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccountData(String key, String value) {
+        if (!isLoggedIn()) return;
+
+        accountManager.setUserData(account, key, value);
+    }
+
+    public void setAccountData(AccountData accountData) {
+        if (!isLoggedIn()) return;
+
+        Bundle bundle = accountData.bundle;
+        Set<String> keySet = bundle.keySet();
+        for (String key : keySet) {
+            accountManager.setUserData(account, key, bundle.getString(key));
+        }
+    }
+
+    @NonNull
+    public String getAccountData(String key) {
+        if (!isLoggedIn()) return "";
+
+        String data = accountManager.getUserData(account, key);
+
+        if (data == null) return "";
+        return data;
     }
 
     @Override
     @NonNull
     public String getAccessToken() throws IOException {
-        if (account == null) return "";
+        if (!isLoggedIn()) return "";
 
         try {
             return accountManager.blockingGetAuthToken(account, Token.BEARER, false);
@@ -56,7 +105,7 @@ public class OAuthAccountManager implements AccountAuthenticator {
     @Override
     @NonNull
     public String getNewAccessToken(String invalidAccessToken) throws IOException {
-        if (account == null) return "";
+        if (!isLoggedIn()) return "";
 
         accountManager.invalidateAuthToken(account.type, invalidAccessToken);
         return getAccessToken();
